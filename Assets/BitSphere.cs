06a22a49fb18afb97cacc745c25b1dfbfe77be6d
KhimichMax <macsim.himich@yandex.ru>
2023-11-15 15:27:29 +0300

    Help

---
+++ Assets/BitSphere.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitSphere : MonoBehaviour
+{
+    private Rigidbody obj;
+    private bool _flag;
+    
+    void Start()
+    {
+        obj = GetComponent<Rigidbody>();
+        
+    }
+
+    private void X()
+    {
+        obj.AddForce(0, 0, 10, ForceMode.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_flag)
+        {
+            Invoke("X", 1f);
+            _flag = true;
+        }
+    }
+}
